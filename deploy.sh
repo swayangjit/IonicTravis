6ba4c3498b274522042a7c839b0695894a14ee79
swayangjit <swayangjit029@gmail.com>
2018-08-13 19:00:26 +0530

    Updated script

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,11 @@ if $UPLOAD_TO_CRASHLYTICS ; then
 	cp ../../config/fabric.properties app/
     	./gradlew  crashlyticsUploadDistributionDevRelease
 fi
+
+#!/bin/bash
+if [ "$UPLOAD_TO_CRASHLYTICS" == "true" ]; then 
+cd platforms/android 
+cp ../../config/fabric.properties app/
+./gradlew  crashlyticsUploadDistributionDevRelease
+fi
+
