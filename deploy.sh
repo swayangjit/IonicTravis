f05e7abd96347e97b3eb8bc4bc5182fb8524707b
swayangjit <swayangjit029@gmail.com>
2018-08-13 23:59:05 +0530

    Updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-if $UPLOAD_TO_CRASHLYTICS ; then
-	cd platforms/android 
-	cp ../../config/fabric.properties app/
-    	./gradlew  crashlyticsUploadDistributionDevRelease
-fi
-
-#!/bin/bash
-if [ "$UPLOAD_TO_CRASHLYTICS" == "true" ]; then 
+if [ "$RELEASE_CANDIDATE" == "$TRAVIS_BRANCH" ]; then 
 cd platforms/android 
 cp ../../config/fabric.properties app/
 ./gradlew  crashlyticsUploadDistributionDevRelease
